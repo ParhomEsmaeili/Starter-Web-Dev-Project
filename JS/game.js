71341ffd9766c0dd2e479a3890efc2b524810a05
ParhomEsmaeili <87826935+ParhomEsmaeili@users.noreply.github.com>
2023-01-29 20:23:14 +0000

    Add the collision event, how it should move in response in reaction to the direction of the player etc

--- JS/game.js
+++ JS/game.js
@@ -17,12 +17,12 @@ window.addEventListener('load', function(){
             //this.position = position;
             //this.velocity = velocity;
             this.game = game;
-            this.characterWidth = 50;
-            this.characterHeight = 100;
+            this.game.characterWidth = 50;
+            this.game.characterHeight = 100;
         }
         drawCharacter(){
             ctx.fillStyle = 'red';
-            ctx.fillRect(this.game.playerPosition.x, this.game.playerPosition.y, this.characterWidth, this.characterHeight);
+            ctx.fillRect(this.game.playerPosition.x, this.game.playerPosition.y, this.game.characterWidth, this.game.characterHeight);
 
         }
         characterUpdate(){
@@ -30,12 +30,12 @@ window.addEventListener('load', function(){
             //console.log(this.game.playerKeys);
 
             //ADD COLLISION CONDITION WITH THE GROUND.
-            if (this.game.playerKeys.includes('w')) this.game.playerVelocity.y = -2;
-            else if(this.game.playerKeys.includes('s')) this.game.playerVelocity.y = 2;
-            //else this.game.playerVelocity.y = 0;
-
-            if (this.game.playerKeys.includes('d')) this.game.playerVelocity.x = 2;
-            else if(this.game.playerKeys.includes('a')) this.game.playerVelocity.x = -2;
+            if (this.game.playerKeys.includes('w') && this.game.playerPosition.y > 0) this.game.playerVelocity.y = -2;
+            else if(this.game.playerKeys.includes('s') && this.game.playerPosition.y <= this.game.height - this.game.characterHeight) this.game.playerVelocity.y = 2;
+            else this.game.playerVelocity.y = 0;
+            
+            if (this.game.playerKeys.includes('d') && this.game.playerPosition.x <= this.game.width - this.game.characterWidth) this.game.playerVelocity.x = 2;
+            else if(this.game.playerKeys.includes('a') && this.game.playerPosition.x > 0) this.game.playerVelocity.x = -2;
             else this.game.playerVelocity.x = 0; 
 
             //This will later be replaced with velocities given by some function of the user input. 
@@ -43,12 +43,82 @@ window.addEventListener('load', function(){
             this.game.playerPosition.y += this.game.playerVelocity.y; 
 
             //Prevent it from going outside of the canvas, we will use a max function that limits the position coord:
-            if (this.game.playerPosition.x + this.characterWidth>= canvas.width){
-                this.game.playerVelocity.x = 0;
-            }
+            /*if (this.game.playerPosition.x + this.characterWidth>= canvas.width){
+                this.game.playerVelocity.x = 0}
+            else if(this.game.playerPosition.x <= 0){this.game.playerVelocity.x = 0};
+
             if (this.game.playerPosition.y + this.characterHeight >= canvas.height){
                 this.game.playerVelocity.y = 0;
-            }  else this.game.playerVelocity.y += gravity; 
+            }  else this.game.playerVelocity.y += gravity; */
+        }
+    }
+    class bulletInstant{
+        constructor(game, bulletPosition, shotDamage, shotSpeed){
+            this.game = game;
+            this.bulletPosition = bulletPosition;
+            this.shotDamage = shotDamage;
+            this.shotSpeed = shotSpeed;
+            this.bulletWidth = 20;
+            this.bulletHeight = 5;
+        }
+        drawBullet(){
+            ctx.fillStyle = 'white';
+            ctx.fillRect(this.bulletPosition.x, this.bulletPosition.y, this.bulletWidth, this.bulletHeight);
+            this.bulletPosition.x += this.shotSpeed; 
+        }
+        bulletCollision(enemy){
+            if (
+                this.bulletPosition.x < enemy.x + enemy.width &&
+                this.bulletPosition.x + this.width > enemy.x &&
+                this.bulletPosition.y < enemy.y + enemy.height &&
+                this.bulletPosition.y + this.height > enemy.y
+            ) {
+                enemy.takeDamage(this.shotDamage);
+                return true;
+            }
+            return false;
+            }
+
+    }
+    class bulletManagement{
+        constructor(game){
+            this.game = game;
+            this.bulletPosition = {
+                x: this.game.playerPosition.x + this.game.characterWidth,
+                y: this.game.playerPosition.y + this.game.characterHeight/2
+            }
+            this.shotDelay = 10;
+            this.shotDamage = 1;
+            this.shotSpeed = 2;
+            this.bullets = [];
+            this.bulletTimer = 0;
+        }
+        shoot(){
+            if (this.bulletTimer <= 0){
+                this.bullets.push(new bulletInstant(this.game, this.bulletPosition, this.shotDamage, this.shotSpeed));
+                this.bulletTimer = this.shotDelay;
+            }
+            this.bulletTimer--;
+        }
+        drawBullet(){
+            this.bullets.forEach((bullet) => {
+                if (this.offScreen(bullet)){
+                    this.bullets.splice(this.bullets.indexOf(bullet), 1);
+                }
+                else bullet.draw(this.game.ctx);
+            })
+        }
+        offScreen(bullet){
+            return (bullet.position.x <= 0 || bullet.position.x >= this.game.width || bullet.position.y <= 0 || bullet.position.y >= this.game.height)
+        }
+        collisionEvent(enemy){
+            return this.bullets.some((bullet) =>{
+                if (bullet.bulletCollision(enemy)){
+                    this.bullets.splice(this.bullets.indexOf(bullet), 1)
+                    return true;
+                }
+                else return false; 
+            })
         }
     }
 
@@ -65,7 +135,9 @@ window.addEventListener('load', function(){
                 if (( (keyEvent.key == 'w') ||
                     (keyEvent.key == 'a') ||
                     (keyEvent.key == 's') ||
-                    (keyEvent.key == 'd') 
+                    (keyEvent.key == 'd') ||
+                    (keyEvent.key == 'o') ||
+                    (keyEvent.key == 'p')
                     )   && this.game.playerKeys.indexOf(keyEvent.key) == -1){
                             this.game.playerKeys.push(keyEvent.key);
                 };
@@ -83,7 +155,7 @@ window.addEventListener('load', function(){
 
     }
     class game {
-        constructor(width, height){
+        constructor(width, height, ctx){
             this.width = width;
             this.height = height;
             this.playerPosition = {
@@ -94,19 +166,27 @@ window.addEventListener('load', function(){
                 x:0,
                 y:0
             };
+            this.characterWidth = 50;
+            this.characterHeight = 100;
             this.playerOne = new player(this);
             this.playerInput = new userInputs(this);
             this.playerKeys = [];
+            this.ctx = ctx;
+            this.playerDirection = null; 
         }
         gameUpdate(){
-            ctx.fillStyle = 'blue';
-            ctx.fillRect(0,0, this.width, this.height);
+            this.ctx.fillStyle = 'blue';
+            this.ctx.fillRect(0,0, this.width, this.height);
             
             this.playerOne.characterUpdate(); 
             this.playerInput.inputListener();
+            if (! this.playerKeys.length()){
+                this.playerDirection = this.playerKeys.slice(-1)
+            };
+            
         }
     }
-    const gravity = 0.1;
+    const gravity = 0; //0.1;
 
     const gameConstruct = new game(canvas.width, canvas.height, ctx);
 
